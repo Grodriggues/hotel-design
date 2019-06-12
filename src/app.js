4a7ed389d9b2c6f0f85e0c219c13daf8f4bc73d9
Gabriel <gabrieloliveira12488@gmail.com>
2019-06-12 15:51:07 -0300

    added the express server

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const public = path.join(__dirname,"../public");
+
+app.use(express.static(public));
+
+app.listen(3000, ()=> console.log("running"));
